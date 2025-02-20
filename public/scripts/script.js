3b5d9d1ce39c5ff79544c1153ae2b0adefde537b
Bouldin <gbouldin@una.edu>
2025-02-20 14:59:46 -0600

    Code for dev item

---
+++ public/scripts/script.js
@@ -0,0 +1,8 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+  
+  //more code here...
+});
+
+console.log("This is Garrett Bouldin\'s Web Profile");
